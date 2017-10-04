3b4d0b69d5b0b4a7dac26627baf9a67175d74a8c
BT_1N3_05 <BT_1N3_05@BT_1N3_05.bktech.local>
2017-10-04 12:58:53 -0400

    Runner

--- ChatBot/src/ChatBotRunner.java
+++ ChatBot/src/ChatBotRunner.java
@@ -1,5 +1,34 @@
+import java.util.Scanner;
 
 public class ChatBotRunner 
 {
-
+	public static void main(String[] args)
+	{
+		
+		System.out.println ("How are you?");
+		Scanner in = new Scanner (System.in);
+		String statement = in.nextLine();
+		System.out.println ("Do you want to talk about movies, music, or TV shows?");
+		System.out.println("Please respond with movies, music, or tv.");
+		String response = in.nextLine();
+		if (response.toLowerCase().equals("movies"))
+		{
+			ChatBotHuang chatbot1 = new ChatBotHuang();
+		}
+		if (response.toLowerCase().equals("music"))
+		{
+			ChatBotRavvin chatbot1 = new ChatBotRavvin();
+		}
+		if (response.toLowerCase().equals("tv"))
+		{
+			ChatBotHasan chatbot1 = new ChatBotHasan();
+		}
+		
+		while (!statement.equals("Bye"))
+		{
+			System.out.println (chatbot1.getResponse(statement));
+			statement = in.nextLine();
+		}
+	}
+	
 }
