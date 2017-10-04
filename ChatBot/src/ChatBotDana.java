e44b308fac6169d93b4e12a6a55a30636c109906
BT_1N3_04 <BT_1N3_04@BT_1N3_04.bktech.local>
2017-10-04 13:16:22 -0400

    updated ChatBotDana

--- ChatBot/src/ChatBotDana.java
+++ ChatBot/src/ChatBotDana.java
@@ -4,15 +4,18 @@ public class ChatBotDana {
 
 	String refPronoun = "";
 	Random r = new Random();
-	//working on the music topic
+	
+	//creates an array of random topics to talk about with the user
 	private String [] randomMusicTopic = {"band", "song", "genre", "artist"};
 	
+	//gets a question to ask the user to begin the conversation in the music subtopic
 	private String getRandomQuestion()
 	{
 		chosenMusicTopic = randomMusicTopic[r.nextInt(randomMusicTopic.length)];
 		System.out.println("What is your favorite " + chosenMusicTopic + "?");
 	}
 	
+	//gets the pronoun to use when responding 
 	if (chosenMusicTopic == "band" || chosenMusicTopic == "artist")
 	{
 		refPronoun = "them";
@@ -21,10 +24,14 @@ public class ChatBotDana {
 	{
 		refPronoun = "that";
 	}
+	
+	//creates an array of possible random responses
 	private String [] randomResponses = {"I also love", "I also like", "Really? Why?", "Can you tell me why?"}
 	
+	//gets a response to send to the user 
 	private String getResponse(String statement)
 	{
+		statement = statement.trim();
 		String response = "";
 		String chosenResponse = randomResponses[r.nextInt(randomResponses.length)];
 		
@@ -36,5 +43,6 @@ public class ChatBotDana {
 		{
 			response = chosenResponse + " " + refPronoun; 
 		}
+		return response; 
 	}
 }
