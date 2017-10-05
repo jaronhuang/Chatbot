eb200722aa04e1b6efcf42a07f2a12bd5569d0b6
BT_1N3_06 <BT_1N3_06@BT_1N3_06.bktech.local>
2017-10-05 12:12:58 -0400

    Created findKeyword

--- ChatBot/src/ChatBotHasan.java
+++ ChatBot/src/ChatBotHasan.java
@@ -10,21 +10,57 @@ public class ChatBotHasan
 		String testResponse=response.toLowerCase();
 		String testKeyword=keyword.toLowerCase();
 		int where=testResponse.indexOf(testKeyword, startPos);
+		String beforeThing="";
+		String afterThing="";
 		if(where>0)
 		{
 			boolean checking=true;
 			while(checking)
 			{
-				if(testResponse.substring(where+testKeyword.length(), where+testKeyword.length()+1).compareTo("a")<0 && testResponse.substring(where+testKeyword.length(), where+testKeyword.length()+1).compareTo("z")>0)
+				beforeThing=testResponse.substring(where-1, where);
+				afterThing=testResponse.substring(where+testKeyword.length(),where+testKeyword.length()+1);
+				if((beforeThing.compareTo("a")<0||beforeThing.compareTo("z")>0)&&(afterThing.compareTo("a")<0||afterThing.compareTo("z")>0))
 				{
+					checking=false;
 					return where;
 				}
 				else
 				{
-					return -1;
+					where=testResponse.indexOf(testKeyword,where);
 				}
 			}
 		}
-		return -1;
+		if(where==0)
+		{
+			boolean checking=true;
+			while(checking)
+			{
+				afterThing=testResponse.substring(where+testKeyword.length(),where+testKeyword.length()+1);
+				if(afterThing.compareTo("a")<0||afterThing.compareTo("z")>0)
+				{
+					checking=false;
+					return where;
+				}
+				else
+				{
+					where=testResponse.indexOf(testKeyword,where);
+					while(checking)
+					{
+						beforeThing=testResponse.substring(where-1, where);
+						afterThing=testResponse.substring(where+testKeyword.length(),where+testKeyword.length()+1);
+						if((beforeThing.compareTo("a")<0||beforeThing.compareTo("z")>0)&&(afterThing.compareTo("a")<0||afterThing.compareTo("z")>0))
+						{
+							checking=false;
+							return where;
+						}
+						else
+						{
+							where=testResponse.indexOf(testKeyword,where);
+						}
+					}
+				}
+			}
+		}
+		return where;
 	}
 }
