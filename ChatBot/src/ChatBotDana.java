f4ddaca7a734ffb43a33346e56e07dcba6b1ae37
BT_1N3_01 <BT_1N3_01@BT_1N3_01.bktech.local>
2017-10-05 12:57:20 -0400

    Updated

--- ChatBot/src/ChatBotDana.java
+++ ChatBot/src/ChatBotDana.java
@@ -1,5 +1,11 @@
 import java.util.Random;
 
+/*
+ * Written by Dana Ravvin
+ * 10/5/17
+ * My music Chatbot
+ */
+
 public class ChatBotDana {
 
 	int emotion = 0;
@@ -7,7 +13,7 @@ public class ChatBotDana {
 	public String getGreeting()
 	{
 		System.out.println("Welcome, I'm the music chatbot!");
-		getRandomQuestion();
+		return getResponse;
 	}
 	
 	String refPronoun = "";
@@ -17,24 +23,21 @@ public class ChatBotDana {
 	private String [] randomMusicTopic = {"band", "song", "genre", "artist"};
 	
 	//gets a question to ask the user to begin the conversation in the music subtopic
-	private String getRandomQuestion()
+	private String getRandomStarterQuestion()
 	{
 		String chosenMusicTopic = randomMusicTopic[r.nextInt(randomMusicTopic.length)];
 		return "What is your favorite " + chosenMusicTopic + "?";
 	}
 	
-	//gets the pronoun to use when responding 
-	if (chosenMusicTopic == "band" || chosenMusicTopic == "artist")
+	private String [] randomQuestions = {"Who would you say is the most popular artist of this decade?", "What is the best way to listen to music?" , "Have you been to any concerts?", "Do you like 		singing?", "What's the best genre?", "Is there anyone in the world who doesn't like music?"};
+	
+	private String getRandomQuestion()
 	{
-		refPronoun = "them";
-	}
-	else
-	{
-		refPronoun = "that";
+		return randomQuestions[r.nextInt(randomQuestions.length)];
 	}
 	
 	//creates an array of possible random responses
-	private String [] randomPositiveResponses = {"I also love " + refPronoun + "." , "I also like " +refPronoun + ".", "It's the best!", "I agree!"};
+	private String [] randomPositiveResponses = {"I also love " + refPronoun + "." , "I also like " +refPronoun + ".", "It's the best!", "I agree!", "That's cool!"};
 	private String [] randomNegativeResponses = {"Oh, I hate " + refPronoun + "." , "Really? " + refPronoun + "? I disagree.", "That's terrible sense of taste." , "Ew!"};
 	private String [] randomNeutralResponses = {"Oh, please, tell me more." , "Why do you think that is?" , "How interesting.", "What else do you like?"};
 	//gets a response to send to the user 
