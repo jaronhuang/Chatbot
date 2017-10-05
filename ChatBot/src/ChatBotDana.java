ce7cd59fe2fdcd8409be68e07446e216d3589ed3
BT_1N3_01 <BT_1N3_01@BT_1N3_01.bktech.local>
2017-10-05 12:41:58 -0400

    updated

--- ChatBot/src/ChatBotDana.java
+++ ChatBot/src/ChatBotDana.java
@@ -2,6 +2,14 @@ import java.util.Random;
 
 public class ChatBotDana {
 
+	int emotion = 0;
+	
+	public String getGreeting()
+	{
+		System.out.println("Welcome, I'm the music chatbot!");
+		getRandomQuestion();
+	}
+	
 	String refPronoun = "";
 	Random r = new Random();
 	
@@ -11,8 +19,8 @@ public class ChatBotDana {
 	//gets a question to ask the user to begin the conversation in the music subtopic
 	private String getRandomQuestion()
 	{
-		chosenMusicTopic = randomMusicTopic[r.nextInt(randomMusicTopic.length)];
-		System.out.println("What is your favorite " + chosenMusicTopic + "?");
+		String chosenMusicTopic = randomMusicTopic[r.nextInt(randomMusicTopic.length)];
+		return "What is your favorite " + chosenMusicTopic + "?";
 	}
 	
 	//gets the pronoun to use when responding 
@@ -26,14 +34,29 @@ public class ChatBotDana {
 	}
 	
 	//creates an array of possible random responses
-	private String [] randomResponses = {"I also love", "I also like", "Really? Why?", "Can you tell me why?"}
-	
+	private String [] randomPositiveResponses = {"I also love " + refPronoun + "." , "I also like " +refPronoun + ".", "It's the best!", "I agree!"};
+	private String [] randomNegativeResponses = {"Oh, I hate " + refPronoun + "." , "Really? " + refPronoun + "? I disagree.", "That's terrible sense of taste." , "Ew!"};
+	private String [] randomNeutralResponses = {"Oh, please, tell me more." , "Why do you think that is?" , "How interesting.", "What else do you like?"};
 	//gets a response to send to the user 
 	private String getResponse(String statement)
 	{
+		String chosenResponse = "";
+		
+		if (emotion > 0)
+		{
+			chosenResponse = randomPositiveResponses[r.nextInt(randomPositiveResponses.length)];
+		}
+		if (emotion < 0)
+		{
+			chosenResponse = randomNegativeResponses[r.nextInt(randomNegativeResponses.length)];
+		}
+		else 
+		{
+			chosenResponse = randomNeutralResponses[r.nextInt(randomNeutralResponses.length)];
+		}
+		
 		statement = statement.trim();
 		String response = "";
-		String chosenResponse = randomResponses[r.nextInt(randomResponses.length)];
 		
 		if (statement.length() == 0)
 		{
@@ -41,7 +64,7 @@ public class ChatBotDana {
 		}
 		else
 		{
-			response = chosenResponse + " " + refPronoun; 
+			response = chosenResponse + "."; 
 		}
 		return response; 
 	}
